8d5bc6d8aafa28bdbf416fbea813544fd23a3ee3
gam0022 <gam0022@gmail.com>
2019-08-25 00:47:50 +0900

    Fix optixPathTracer

--- optixPathTracer/common.h
+++ optixPathTracer/common.h
@@ -28,14 +28,21 @@
 
 #pragma once
 
-#include <optixu/optixu_math_namespace.h>                                        
-#include "common.h"
+// Common optix::Ray types
+#define RADIANCE_RAY_TYPE 0
+#define SHADOW_RAY_TYPE 1
 
-struct ParallelogramLight                                                        
-{                                                                                
-    optix::float3 corner;                                                          
-    optix::float3 v1, v2;                                                          
-    optix::float3 normal;                                                          
-    optix::float3 emission;                                                        
-};                                                                               
+#include <optixu/optixu_vector_types.h>
+
+struct BasicLight
+{
+#if defined(__cplusplus)
+  typedef optix::float3 float3;
+#endif
+  float3 pos;
+  float3 color;
+  int    casts_shadow; 
+  int    padding;      // make this structure 32 bytes -- powers of two are your friend!
+};
+
 
